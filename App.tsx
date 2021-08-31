b174b0b2225dc632b42b9aa3356c2d078a1fd052
Felipe Chernicharo <string7dev@gmail.com>
2021-08-31 18:17:32 -0400

    toggling theme

--- App.tsx
+++ App.tsx
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, StatusBarStyle } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [theme, setTheme] = useState('dark');
+	const [statusBarTheme, setStatusBarTheme] =
+		useState<StatusBarStyle>('light');
+
+	function handleThemeToggle() {
+		setTheme(t => (t === 'dark' ? 'light' : 'dark'));
+		setStatusBarTheme(t => (t === 'dark' ? 'light' : 'dark'));
+	}
+
+	return (
+		<View style={styles(theme).container}>
+			<Text style={styles(theme).text}>
+				Open up App.tsx to start working on your app!
+			</Text>
+			<TouchableOpacity
+				style={styles(theme).button}
+				onPress={handleThemeToggle}
+			>
+				<Text style={styles(theme).buttonText}>Toggle theme</Text>
+			</TouchableOpacity>
+			<StatusBar style={statusBarTheme} />
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = (colorScheme: string) =>
+	StyleSheet.create({
+		container: {
+			flex: 1,
+			backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
+			color: colorScheme === 'dark' ? '#fff' : '#000',
+			alignItems: 'center',
+			justifyContent: 'center',
+		},
+		text: {
+			color: colorScheme === 'dark' ? '#fff' : '#000',
+		},
+		button: {
+			backgroundColor: '#e3ba25',
+			padding: 6,
+		},
+		buttonText: {
+			fontSize: 20,
+			color: colorScheme === 'dark' ? '#000' : '#fff',
+		},
+	});
